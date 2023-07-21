b16428a41243ab6a076bb6d77929c869b5d8d48e
David Garcia Varas <davidga2@c1r9s6.42madrid.com>
2023-07-21 08:11:15 +0200

    valid args chars ft

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,16 +6,51 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 01:41:19 by davidga2          #+#    #+#             */
-/*   Updated: 2023/07/17 02:49:26 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/07/21 08:03:47 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_arg_cheker(char *argc[])
-{
+#include "../utils/push_swap.h"
 
+int	ft_comp_args_valid_chars(char **m)
+{
+	t_iter i;
+
+	i.i = 1;
+	i.j = 0;
+	while(m[i.i])
+	{
+		if(!ft_isdigit(m[i.i][i.j]) && (m[i.i][0] != '-' || m[i.i][1] == '\0'))
+			return (ft_printf("[x] Caracter inválido: %c\n", m[i.i][i.j]), 0);
+		i.j++;
+		while (m[i.i][i.j])
+		{
+			if(!ft_isdigit(m[i.i][i.j]))
+				return (ft_printf("[x] Caracter inválido: %c\n", m[i.i][i.j]), 0);
+			i.j++;
+		}
+		i.j = 0;
+		i.i++;
+	}
+	return (ft_printf("[v] Argumentos válidos.\n"), 1);
 }
 
-void	main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
+	t_list	*a;
+	t_list	*b;
+
+	a = NULL;
+	b = NULL;
+	int		i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_printf("argv[%i]: %s\n", i, argv[i]);
+		i++;
+	}
+	if (!ft_comp_args_valid_chars(argv))
+			return (0);
 	return (0);
 }
--- utils/push_swap.h
+++ utils/push_swap.h
@@ -6,10 +6,25 @@
 /*   By: davidga2 <davidga2@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 18:07:46 by davidga2          #+#    #+#             */
-/*   Updated: 2023/07/16 18:12:46 by davidga2         ###   ########.fr       */
+/*   Updated: 2023/07/19 18:36:11 by davidga2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../libft/libft.h"
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+typedef struct s_iters{
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	size_t	l;
+	size_t	m;
+}			t_iter;
+
+void	ft_pa(t_list **lst_a, t_list **lst_b);
+void	ft_pb(t_list **lst_b, t_list **lst_a);
+void	ft_rra(t_list **lst_a);
+int		main(int argc, char *argv[]);
+#endif
